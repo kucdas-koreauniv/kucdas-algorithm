d23b98c811dca260efd0cb104798573152387ef6
uichan129 <ouroborosuichan@gmail.com>
2025-07-03 16:18:31 +0900

    1주차 숙제 재제출합니당

--- homework/week1/2025320129-uichan/소스.c
+++ homework/week1/2025320129-uichan/소스.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    int input_a, input_b;
+    int input_a, input_b; //값 2개 입력 받기
     scanf("%d %d", &input_a, &input_b);
     printf("%d", input_a + input_b); 
 
