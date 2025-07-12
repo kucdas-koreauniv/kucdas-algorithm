486a6d1179b3dff4ee9dc8f9a143c7e80459b8e5
chaewon6050 <chaewon6050@gmail.com>
2025-07-13 00:26:12 +0900

    <The Josephus Problem.c 수정사항> 1. 변수명 수정 : N=>num_people, K=>step_size, num=>index 2. malloc으로 선언한 메모리 해제 3. for문 처음이나 마지막 index만 특별하게 동작시키기 위해 if문을 넣는 것을 지양 -> if문 삭제, 수정

--- homework/week2/2025320103-hwangchaewon/The Josephus Problem.c
+++ homework/week2/2025320103-hwangchaewon/The Josephus Problem.c
@@ -1,52 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct person{
-    int num;
-    struct person* p_next;
-}Person;
+typedef struct person
+{
+    int index;
+    struct person *p_next;
+} Person;
 
-Person* head = NULL;
-Person* tail = NULL;
+Person *head = NULL;
+Person *tail = NULL;
 
 int main()
 {
-    int N, K;
-    scanf("%d %d", &N, &K);
+    int num_people, step_size;
+    scanf("%d %d", &num_people, &step_size);
 
-    for (int i = 0; i < N; i++) {
-        Person* p = (Person*)malloc(sizeof(Person));
+    for (int i = 0; i < num_people; i++)
+    {
+        Person *p = (Person *)malloc(sizeof(Person));
         p->p_next = NULL;
-        p->num = i + 1;
-        if (head == NULL) {
+        p->index = i + 1;
+        if (head == NULL)
+        {
             head = p;
             tail = p;
         }
-        else {
+        else
+        {
             tail->p_next = p;
             tail = p;
         }
     }
     tail->p_next = head; // 원형 큐??
-    
-    Person* p = tail;
-    Person* prev = NULL;
+
+    Person *p = tail;
+    Person *prev = NULL;
 
     printf("<");
-    for (int i = 0; i < N; i++) {
-        if(i != 0) printf(", ");
-        for (int r = 0; r < K; r++) {
+
+    for (int r = 0; r < step_size; r++)
+    {
+        prev = p;
+        p = p->p_next;
+    }
+    // 제거
+    printf("%d", p->index);
+    prev->p_next = p->p_next;
+
+    for (int i = 1; i < num_people; i++)
+    {
+        for (int r = 0; r < step_size; r++)
+        {
             prev = p;
             p = p->p_next;
         }
-        //제거
-        printf("%d", p->num);
+        // 제거
+        printf(", %d", p->index);
         prev->p_next = p->p_next;
-
     }
     printf(">");
 
-
     return 0;
-
 }
