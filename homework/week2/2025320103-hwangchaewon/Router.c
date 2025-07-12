cb7e198a4016673b5abea76034b281c286d1b430
chaewon6050 <chaewon6050@gmail.com>
2025-07-12 23:41:35 +0900

    <Router.c 수정사항> 1. 변수명 변경 : N=>buffer_size, size->count (모두 대문자인 변수 사용 조심!) 2. indent 맞추기 3. while문 안에서만 사용되는 변수 안에서 선언 4. enqueue, dequeue (+dumpqueue) 함수로 빼기 5. 비어있는 if문, 복잡한 if문(switch문 이용) 수정

--- homework/week2/2025320103-hwangchaewon/Router.c
+++ homework/week2/2025320103-hwangchaewon/Router.c
@@ -1,69 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct q {
+typedef struct q
+{
     int data;
-    struct q* p_next;
-}Queue;
+    struct q *p_next;
+} Queue;
 
-Queue* front = NULL;
-Queue* rear = NULL;
+Queue *front = NULL;
+Queue *rear = NULL;
 
-int main() {
-    int N; 
-    scanf("%d",&N);
+void Dequeue(Queue **p_front, int *p_count)
+{
+    Queue *p = *p_front;
+    *p_front = (*p_front)->p_next;
+    free(p);
+    (*p_count)--;
+}
 
-    int temp = 0;
-	int size = 0;
+void Enqueue(Queue **pp_buffer, int temp, int *p_count, Queue **p_front, Queue **p_rear)
+{
 
-    while (1) {
+    (*pp_buffer)->data = temp;
+    (*p_count)++;
+    if (*p_front == NULL)
+    { // 비어있을 때
+        *p_front = *pp_buffer;
+        *p_rear = *pp_buffer;
+    }
+    else
+    {
+        (*p_rear)->p_next = *pp_buffer;
+        *p_rear = *pp_buffer;
+    }
+}
+
+void dumpQueue(int count, Queue **p_front)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Queue *p = *p_front;
+        printf("%d ", p->data);
+        *p_front = (*p_front)->p_next; // front를 다음 노드로 이동
+        free(p);                       // 현재 노드 메모리 해제
+    }
+}
+
+int main()
+{
+    int buffer_size;
+    scanf("%d", &buffer_size);
+
+    int count = 0;
+
+    while (1)
+    {
+        int temp = 0;
 
         scanf("%d", &temp);
 
- 
-        if (temp == 0) { // 패킷 하나 처리
-            Queue* p = front;
-			front = front->p_next;
-            free(p);
-            size--;
-        }
-        else if (temp == -1) { // 입력의 끝
+        if (temp == -1)
+        {
             break;
         }
-        else {
 
-            if (size >= N) { // 큐가 가득 찼을 때
-            }
-            else {
-                Queue* p_buffer = (Queue*)malloc(sizeof(Queue));
+        switch (temp)
+        {
+        case 0:
+            Dequeue(&front, &count);
+            break;
+        default:
+            if (count < buffer_size)
+            {
+                Queue *p_buffer = (Queue *)malloc(sizeof(Queue));
                 p_buffer->p_next = NULL;
 
-                p_buffer->data = temp;
-                size++;
-                if (front == NULL) { // 비어있을 때
-                    front = p_buffer;
-                    rear = p_buffer;
-                }
-                else {
-                    rear->p_next = p_buffer;
-                    rear = p_buffer;
-                }
+                Enqueue(&p_buffer, temp, &count, &front, &rear);
             }
-
-            
         }
-
     }
 
-    for (int i = 0; i < size; i++) {
-        Queue* p = front;
-		printf("%d ", p->data);
-        front = front->p_next; // front를 다음 노드로 이동
-        free(p); // 현재 노드 메모리 해제
-    }
-	
-
-
+    dumpQueue(count, &front);
 
     return 0;
 }
