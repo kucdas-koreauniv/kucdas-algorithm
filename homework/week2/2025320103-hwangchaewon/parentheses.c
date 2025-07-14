2bdcf45e043ded4d3b8c1d16b2ed5f7366487076
chaewon6050 <chaewon6050@gmail.com>
2025-07-14 20:56:11 +0900

    <parentheses.c 수정사항> 1. 변수명 수정 T=>num_testcase 2. indent 맞추기 3. 조건문에 있는 strlen 삭제, for문 전에 계산해서 buffer_len에 넣어두기 (iteration 돌때마다 strlen이 작동해서 비효율적이기 때문) 4. if문에서 break 했다면 else문으로 감쌀 필요x 5. for문 안에 있을 필요 없는 부분 빼내기 6. 4번 5번 변경사항으로 인한 오류 수정 (break가 포함된 if문을 뒤쪽으로 배치함)

--- homework/week2/2025320103-hwangchaewon/parentheses.c
+++ homework/week2/2025320103-hwangchaewon/parentheses.c
@@ -4,41 +4,49 @@
 int main()
 {
     // 입력 : T개의 테스트 데이터
-    int T;
-    scanf("%d", &T);
+    int num_testcase;
+    scanf("%d", &num_testcase);
 
-    for (int i = 0; i < T; i++) {
-        //선언과 동시에 초기화
+    for (int i = 0; i < num_testcase; i++)
+    {
+        // 선언과 동시에 초기화
         int stacksize = 0; // 사이즈 개념만 사용
-        char buffer[51] = { 0, };
-				scanf("%s", buffer);
-        for (int j = 0; j < strlen(buffer); j++) {
-            if (stacksize < 0) {
+        char buffer[51] = {
+            0,
+        };
+        scanf("%s", buffer);
+
+        int buffer_len = strlen(buffer);
+        for (int j = 0; j < buffer_len; j++)
+        {
+            if (buffer[j] == '(')
+            {
+                stacksize++;
+            }
+            else if (buffer[j] == ')')
+            {
+                stacksize--;
+            }
+            else
+            {
                 printf("NO\n");
                 break;
             }
-            else {
-                if (buffer[j] == '(') {
-                    stacksize++;
-                }
-                else if (buffer[j] == ')') {
-                    stacksize--;
-                }
-                else {
-                    printf("NO\n");
-                    break;
-                }
-            }
 
-            if (j == strlen(buffer) - 1) {
-                if (stacksize == 0) {
-                    printf("YES\n");
-                }
-                else {
-                    printf("NO\n");
-                }
-
+            if (stacksize < 0)
+            {
+                printf("NO\n");
+                break;
             }
         }
+        if (stacksize == 0)
+        {
+            printf("YES\n");
+        }
+        else if (stacksize > 0)
+        {
+            printf("NO\n");
+        }
     }
+    return 0;
 }
