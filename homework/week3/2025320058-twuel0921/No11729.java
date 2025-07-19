fbaf94ecc662406a2189a3c34bd4fbc16a05e0ab
Twuel <qkrwjdgns1239@gmail.com>
2025-07-19 15:44:23 +0900

    Z 문제는 조금 더 생각해보고 커밋하겠습니다.

--- homework/week3/2025320058-twuel0921/No11729.java
+++ homework/week3/2025320058-twuel0921/No11729.java
@@ -2,28 +2,24 @@ import java.util.Scanner;
 
 //하노이 탑 이동 순서 풀이 코드
 public class No11729 {
-    private static StringBuilder sb = new StringBuilder();
-
-    private static int count = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        hanoi(n, 1, 3);
-        System.out.println(count);
-        System.out.println(sb.toString());
+        StringBuilder sb = new StringBuilder();
+        System.out.println(hanoi(sb, n, 1, 3, 0));
+        System.out.println(sb);
     }
 
-    private static void hanoi(int n, int from, int to) {
+    private static int hanoi(StringBuilder sb, int n, int from, int to, int count) {
         if(n == 1) {
             sb.append(from).append(" ").append(to).append('\n');
-            count++;
-            return;
+            return count+1;
         }
-        hanoi(n-1, from, 6-from-to);
+        int hanoi = 0;
+        hanoi += hanoi(sb, n-1, from, 6-from-to, count);
         sb.append(from).append(" ").append(to).append('\n');
-        count++;
-        hanoi(n-1, 6-from-to, to);
+        hanoi++;
+        hanoi += hanoi(sb, n-1, 6-from-to, to, count);
+        return hanoi;
     }
 }
