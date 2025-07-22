d45a27deda75bf07902dd89ea333d0839f9c717f
chaewon6050 <chaewon6050@gmail.com>
2025-07-22 12:54:20 +0900

    [2447 - Star pattern 10.c 수정] 1. 함수명 소문자로 시작! Star => star 2. if문 조건 간결하게 수정! 불필요한 변수(two_thirds, real_y, real_x) 삭제 3. 가독성 높이기 : *대신 배열로 접근 4. 2차원 배열 대신 1차원 배열 사용

--- homework/week3/2025320103-hwangchaewon/2447 - Star pattern 10.c
+++ homework/week3/2025320103-hwangchaewon/2447 - Star pattern 10.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-char Star(int one_side, int real_y, int real_x)
+char star(int one_side, int y_index, int x_index)
 {
     if (one_side == 1)
         return '*';
 
-    int one_third = one_side / 3;        // 사각형 한 변의 3분의 1
-    int two_thirds = (one_side / 3) * 2; // 사각형 한 변의 3분의 2
+    int one_third = one_side / 3; // 사각형 한 변의 3분의 1
 
-    if ((real_y > one_third && real_y <= two_thirds) && (real_x > one_third && real_x <= two_thirds))
+    if (y_index / one_third == 1 && x_index / one_third == 1)
     {
         return ' ';
     }
     else
     {
-        return Star(one_third, real_y % one_third, real_x % one_third);
+        return star(one_third, y_index % one_third, x_index % one_third);
     }
 }
 
@@ -26,22 +25,19 @@ int main()
     int num;
     scanf("%d", &num);
 
-    char **pp_star = (char **)malloc(sizeof(char *) * num);
+    char *p_star = (char *)malloc(sizeof(char) * num);
 
     for (int y_index = 0; y_index < num; y_index++)
     {
-        *(pp_star + y_index) = (char *)malloc(sizeof(char) * num + 1);
         for (int x_index = 0; x_index < num; x_index++)
         {
-
-            *(*(pp_star + y_index) + x_index) = Star(num, y_index + 1, x_index + 1); // 0~num-1의 인덱스 말고 1~num의 숫자로 가로 세로 좌표를 받음
+            p_star[x_index] = star(num, y_index, x_index); // 0~num-1의 인덱스 말고 1~num의 숫자로 가로 세로 좌표를 받음
         }
-        *(*(pp_star + y_index) + num) = '\0';
-        printf("%s", *(pp_star + y_index));
+        p_star[num] = '\0';
+        printf(p_star);
         printf("\n");
-        free(*(pp_star + y_index));
     }
-    free(pp_star);
+    free(p_star);
 
     return 0;
 }
