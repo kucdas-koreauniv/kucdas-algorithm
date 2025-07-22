424cd522ccda8f67bc74ec0df3c62e5cf8e9810e
chaewon6050 <chaewon6050@gmail.com>
2025-07-22 12:37:57 +0900

    [17478 - What is Recursive function 수정] 1. string_index 대신 char* 사용

--- homework/week3/2025320103-hwangchaewon/17478 - What is Recursive function.c
+++ homework/week3/2025320103-hwangchaewon/17478 - What is Recursive function.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-char text[5][130] = {"\"재귀함수가 뭔가요?\"\n",
-                     "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",
-                     "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n",
-                     "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n",
-                     "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n"};
+char *str_question = "\"재귀함수가 뭔가요?\"\n";
+char *str_explain1 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+char *str_explain2 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+char *str_explain3 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+char *str_answer = "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
 
 // text[0] => question
 // text[1] => text1
@@ -12,16 +12,16 @@ char text[5][130] = {"\"재귀함수가 뭔가요?\"\n",
 // text[3] => text3
 // text[4] => answer
 
-void sentence(int index, int target_index, char *s)
+void sentence(int index, int target_index, char *str)
 {
     if (index == target_index)
     {
-        printf(s);
+        printf(str);
     }
     else
     {
         printf("____");
-        sentence(index + 1, target_index, s);
+        sentence(index + 1, target_index, str);
     }
 }
 
@@ -45,17 +45,17 @@ void finish(int index, int target_index)
 
 void recursion(int index, int target_index)
 {
-    sentence(index, target_index, text[0]); // question
+    sentence(index, target_index, str_question); // question
     if (index == 0)
     {
-        sentence(index, target_index, text[4]); // answer
+        sentence(index, target_index, str_answer); // answer
         finish(index, target_index);
     }
     else
     {
-        sentence(index, target_index, text[1]);
-        sentence(index, target_index, text[2]);
-        sentence(index, target_index, text[3]);
+        sentence(index, target_index, str_explain1);
+        sentence(index, target_index, str_explain2);
+        sentence(index, target_index, str_explain3);
         recursion(index - 1, target_index);
     }
 }
